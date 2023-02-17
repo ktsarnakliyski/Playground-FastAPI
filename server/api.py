c4e8853af59682433f618d5c6c33da7b27d76150
Kiril Tsarnakliyski <ktsarnakliyski@gmail.com>
2023-02-17 16:14:20 +0200

    Add settings

--- server/api.py
+++ server/api.py
@@ -1,4 +1,3 @@
-import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -17,7 +16,3 @@ def hello_world():
 @app.post("/uppercase")
 def uppercase(request: UppercaseRequest):
     return {"word": request.word.upper()}
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
